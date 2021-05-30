de79e383b7af7af17a6d28b6c5b2023521385ae8
irinaabnizova <61786710+irinaabnizova@users.noreply.github.com>
2021-05-30 14:09:09 +0100

    Update compute_dar_sar_CountsPu_EctEndMes_chr3.m
    Added the writing sub-function to the main compute_DAR_SAR code

--- compute_dar_sar_CountsPu_EctEndMes_chr3.m
+++ compute_dar_sar_CountsPu_EctEndMes_chr3.m
@@ -50,4 +50,26 @@ display('2. DEfine DARs and  equal SARs HL with standard params')
     textFilename=sprintf('SARH_EctEndMes_c25_Pu036_%s.txt',name_chr);
     [chrN]=save_DAR_EctEndMes(SARH,folder,textFilename,chrN);
     
-    
+    %=================================subfunction save DAR
+    function [chrN]=save_DAR_EctEndMes(DAR,folder,textFilename,chrN)
+%===============================write DAR or SAR in the 8 field's file
+%------------------------Input
+ %SAR=[chr st en chp' lev_ES' lev_EnS' lev_CS' ind'];
+    %    1    2  3  4     5        6     7       8
+    chr=DAR(:,1);
+    starts=DAR(:,2)-25;
+    ends=DAR(:,3)+25;
+    ch=DAR(:,4);
+    lev_EcS=DAR(:,5);
+    lev_EnS=DAR(:,6);
+    lev_MS=DAR(:,7);
+    ind=DAR(:,8);
+       
+  %--------------------DARs 8 fileds
+  %textFilename=sprintf('SARL_EctEndMes_c25_aL02_GCPu.txt');
+  fp = fopen(fullfile(folder, textFilename), 'w');
+  for i=1:length(starts),
+       fprintf(fp,'%d\t%d\t%d\t%.2f\t%.2f\t%.2f\t%.2f\t%d\n',chr(i),starts(i),ends(i),ch(i),lev_EcS(i),lev_EnS(i),lev_MS(i),ind(i));
+  end
+  fclose(fp);
+ end
